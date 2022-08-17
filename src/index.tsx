a6260b878469e937eacb359a129bdd48225bbfda
ltsvn <92997990+ltsvn@users.noreply.github.com>
2022-08-17 15:35:12 +0200

    Train Unit tests

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {splitIntoWords} from './01-hello-tests/01'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const sentense = 'Hello my friends;'
+const result = splitIntoWords('')
+console.log(result[0] === 'hello');
+console.log(result[1] === 'my');
+console.log(result[2] === 'friend');
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
