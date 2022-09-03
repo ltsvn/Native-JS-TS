708738e1d9aa4a81e7d69386ff03583e37ab7af8
ltsvn <92997990+ltsvn@users.noreply.github.com>
2022-09-03 23:41:13 +0200

    start to do destructing assignment

--- src/06-destructing-assignment/destructing.test.ts
+++ src/06-destructing-assignment/destructing.test.ts
@@ -1,10 +1,52 @@
+let props: ManType;
+
+
+type ManType= {
+    name:string
+    age:number
+    lessons:Array<LessonType>
+    address:{
+        street:{
+            title:string
+        }
+    }
+}
+
+type LessonType={
+    title: string
+}
+
+beforeEach(()=>{
+     props = {
+        name: 'Dimych',
+        age: 32,
+        lessons: [{title: '1'}, {title: '2'}],
+        address:{
+            street:{
+                title:"yyyyyyy"
+            }
+        }
+    }
+})
+
+
+
+
+
 
 
 test('', () => {
-let man ={
-    name:'Dimych',
-    age:32,
-    lessons:[{title:'1'}, {title:'2'}]
 
-}
+
+    // const age = props.age
+    // const lessons = props.lessons
+    const {age, lessons} = props
+    const {title} =props.address.street
+
+
+    expect(age).toBe(32)
+    expect(lessons.length).toBe(2)
+    expect(title).toBe('yyyyyyy')
+
+
 })
